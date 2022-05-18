3d458faf5e7dd899094dac10d576c2b45c1369ad
abdaldeen <abdelrahman.abdaldeen@mawdoo3.com>
2022-05-18 20:46:30 +0300

    dashboard backend and API (questions)

--- pages/الأسئلة.vue
+++ pages/الأسئلة.vue
@@ -6,13 +6,18 @@
       class="search"
       :class="{ 'text-right': isRTL }"
     />
-    <el-input placeholder="Please input" class="input-with-select">
-    <el-button slot="append" icon="el-icon-search"></el-button>
-  </el-input>
-    <el-table
-      :data="Object.values(data.questions)"
-      style="width: 100%"
+    <el-input
+      placeholder="Please input"
+      class="input-with-select"
+      v-model="search"
     >
+      <el-button
+        slot="append"
+        icon="el-icon-search"
+        @click="handleSearch"
+      ></el-button>
+    </el-input>
+    <el-table :data="questionsArr" style="width: 100%">
       <el-table-column type="expand" :class="{ 'float-right': isRTL }">
         <template slot-scope="props">
           <p align="right">نص السؤال:</p>
@@ -21,36 +26,64 @@
       </el-table-column>
       <el-table-column align="right" label="العنوان" prop="title">
       </el-table-column>
-      <el-table-column align="right" label="عدد الاصوات" prop="votesCount" max-width="70"/>
-      <el-table-column align="right" label="عدد الابلاغات" prop="reportsCount" max-width="70"/>
+      <el-table-column
+        align="right"
+        label="عدد الاصوات"
+        prop="votesCount"
+        max-width="70"
+      />
+      <el-table-column align="right" label="عدد الابلاغات" max-width="70">
+        <template slot-scope="props">
+          <span
+            v-if="props.row.reportsCount != 1000"
+            :class="{ hidedDoc: props.row.reportsCount >= 10 }"
+          >
+            {{ props.row.reportsCount }}
+          </span>
+          <span class="hidedDoc" v-else> تم اخفاء السؤال من قبل مشرف </span>
+        </template>
+      </el-table-column>
       <el-table-column
         align="right"
         label="الاسم"
         prop="userRecord.displayName"
-        
       >
       </el-table-column>
       <el-table-column align="right" min-width="100" label="الاجراءات">
         <template slot-scope="scope" :class="{ 'text-right': isRTL }">
-          <el-button size="mini" @click="handleHide(scope.$index, scope.row)">
-            اخفاء
-          </el-button>
-          <el-button size="mini" @click="handleBlock(scope.$index, scope.row)">
-            حظر السائل
-          </el-button>
-          <el-button
-            size="mini"
-            type="danger"
-            @click="handleDelete(scope.$index, scope.row)"
-            >حذف
-          </el-button>
+          <div :id="'actions-' + scope.$index" class="actionsContainer">
+            <el-button
+              size="mini"
+              @click="(e) => handleHide(e, scope.$index, scope.row)"
+            >
+              {{ scope.row.reportsCount >= 10 ? "اظهار" : "اخفاء" }}
+            </el-button>
+            <el-button
+              size="mini"
+              @click="handleBlock(scope.$index, scope.row)"
+            >
+              {{
+                !scope.row.userRecord.disabled ? "حظر السائل" : "الغاء الحظر"
+              }}
+            </el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)"
+              >حذف
+            </el-button>
+            <el-link :underline="false" :href="'/الإجابات/' + scope.row.qID">
+              الاجابات ->
+            </el-link>
+            <span> انتظر قليلاً ... </span>
+          </div>
         </template>
       </el-table-column>
     </el-table>
   </div>
 </template>
 <script>
-import { Button, Table, TableColumn } from "element-ui";
+import { Button, Table, TableColumn, link } from "element-ui";
 
 export default {
   name: "questions",
@@ -58,78 +91,135 @@ export default {
     [Table.name]: Table,
     [TableColumn.name]: TableColumn,
     [Button.name]: Button,
+    [link.name]: link,
   },
   async asyncData({ $axios, store, error, redirect }) {
     return await $axios
-      .$get('/admin/questions/get',{
-              headers: {
-                authorization: `Bearer ${store.state.user.token}`,
-              },
-            })
+      .$get("/admin/questions/get", {
+        headers: {
+          authorization: `Bearer ${store.state.user.token}`,
+        },
+      })
       .then((r) => {
-        const questions = r.questions
-        const lastKey = r.lastKey
-        store.commit('questions/add', { questions, lastKey })
+        const questions = r.questions;
+        const lastKey = r.lastKey;
+        store.commit("questions/add", { questions, lastKey });
       })
       .catch((e) => {
-        console.error(e)
+        console.error(e);
         // error({ statusCode: 404, message: 'Post not found' });
-      })
+      });
   },
   data() {
     return {
-      tableData: [
-        {
-          title: "عنوان السؤال",
-          votesNum: "100",
-          userName: "محمد احمد",
-          body: "السلام عليكم ",
-        },
-        {
-          title: "2016-05-02",
-          votesNum: "John",
-          userName: "No. 189, Grove St, Los Angeles",
-          body: "السلام عليكم ",
-        },
-        {
-          title: "2016-05-04",
-          votesNum: "Morgan",
-          userName: "No. 189, Grove St, Los Angeles",
-          body: "السلام عليكم ",
-        },
-        {
-          title: "2016-05-01",
-          votesNum: "Jessy",
-          userName: "No. 189, Grove St, Los Angeles",
-          body: "السلام عليكم ",
-        },
-      ],
       search: "",
     };
   },
   methods: {
-    handleHide(index, row) {
-      console.log(index, row);
+    async handleHide(e, index, row) {
+      console.log(index, row, e);
+      console.log(e.target);
+      // e.target.innerHTML = "انتظر..."
+      // e.target.setAttribute("disabled","1")
+      // return;
+      document.querySelector(`#actions-${index}`).classList.add("loading");
+      await this.$axios
+        .$post(
+          "/admin/hideDoc",
+          {
+            dID: row.qID,
+            collection: "questions",
+            reportsCount: row.reportsCount,
+          },
+          {
+            headers: {
+              authorization: `Bearer ${this.$store.state.user.token}`,
+            },
+          }
+        )
+        .then((r) => {
+          console.log(r);
+        })
+        .catch((e) => {
+          console.error(e);
+          // error({ statusCode: 404, message: 'Post not found' });
+        });
+      let reportsCount = row.reportsCount < 10 ? 1000 : 0;
+      this.$store.commit("questions/updateReportsCount", {
+        reportsCount,
+        index,
+      });
+      document.querySelector(`#actions-${index}`).classList.remove("loading");
     },
-    handleBlock(index, row) {
-      console.log(index, row);
+    async handleBlock(index, row) {
+      document.querySelector(`#actions-${index}`).classList.add("loading");
+      let uid = this.questionsArr[index].userRecord.uid;
+      let value = this.questionsArr[index].userRecord.disabled || "";
+      let r = await this.$axios
+        .$get("/admin/blockUser", {
+          params: {
+            uid,
+            value,
+          },
+          headers: {
+            authorization: `Bearer ${this.$store.state.user.token}`,
+          },
+        })
+        .then((r) => {
+          return 1;
+        })
+        .catch((e) => {
+          console.error(e);
+          return 0;
+          // error({ statusCode: 404, message: 'Post not found' });
+        });
+      r && this.$store.commit("questions/blockUser", { index, value });
+      document.querySelector(`#actions-${index}`).classList.remove("loading");
     },
     async handleDelete(index, row) {
+      document.querySelector(`#actions-${index}`).classList.add("loading");
       await this.$axios
-      .$post('/admin/questions/delete',{
-        qID: row.qID
-      },{
-              headers: {
-                authorization: `Bearer ${this.$store.state.user.token}`,
-              },
-            })
-      .then((r) => {
-        console.log(r)
-      })
-      .catch((e) => {
-        console.error(e)
-        // error({ statusCode: 404, message: 'Post not found' });
-      })
+        .$post(
+          "/admin/questions/delete",
+          {
+            qID: row.qID,
+          },
+          {
+            headers: {
+              authorization: `Bearer ${this.$store.state.user.token}`,
+            },
+          }
+        )
+        .then((r) => {
+          document
+            .querySelector(`#actions-${index}`)
+            .closest("tr").style.display = "none";
+          return 1;
+        })
+        .catch((e) => {
+          console.error(e);
+          return 0;
+          // error({ statusCode: 404, message: 'Post not found' });
+        });
+      // true && this.$store.commit('questions/delete',{index})
+    },
+    async handleSearch() {
+      try {
+        let qID = this.search;
+        let r = await this.$axios.$get("/admin/questions/getOne", {
+          params: {
+            qID,
+          },
+          headers: {
+            authorization: `Bearer ${this.$store.state.user.token}`,
+          },
+        });
+        let questions = {};
+        questions[r.question.qID] = r.question;
+        this.$store.commit("questions/add", { questions, lastKey: "" });
+      } catch (error) {
+        console.error(error);
+      }
     },
   },
   mounted() {
@@ -143,7 +233,10 @@ export default {
       return true;
     },
     data() {
-      return this.$store.state.questions.data
+      return this.$store.state.questions.data;
+    },
+    questionsArr() {
+      return Object.values(this.data.questions);
     },
   },
   beforeDestroy() {
@@ -152,7 +245,7 @@ export default {
   },
 };
 </script>
-<style>
+<style lang="scss">
 .search {
   margin: auto;
   margin-bottom: 2rem;
@@ -163,5 +256,21 @@ export default {
 }
 .table-transparent {
   background-color: transparent !important;
+}
+.hidedDoc {
+  color: red;
+}
+.actionsContainer {
+  & > span {
+    display: none;
+  }
+}
+.actionsContainer.loading {
+  & > span {
+    display: block;
+  }
+  button {
+    display: none;
+  }
 }
 </style>
--- store/questions.js
+++ store/questions.js
@@ -1,13 +1,25 @@
 export const state = () => ({
   data: {},
-})
+});
 
 export const mutations = {
   add(state, data) {
-    state.data = data
+    state.data = data;
+    state.data.questionsArr = Object.values(data.questions);
   },
   loadMore(state, data) {
-    state.data.questions = [...state.data.questions, ...data.questions]
-    state.data.lastKey = data.lastKey
+    state.data.questions = { ...state.data.questions, ...data.questions };
+    state.data.lastKey = data.lastKey;
   },
-}
+  updateReportsCount(state, data) {
+    state.data.questionsArr[data.index].reportsCount = data.reportsCount;
+  },
+  delete(state, data) {
+    var newArr = Array.from(state.data.questionsArr);
+    newArr.splice(data.index, 1);
+    state.data.questionsArr = newArr;
+  },
+  blockUser(state, data) {
+    state.data.questionsArr[data.index].userRecord.disabled = !data.value;
+  },
+};
