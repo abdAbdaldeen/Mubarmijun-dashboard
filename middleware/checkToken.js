d5016b5777e0ee0c2e00668418702bb225d44be8
abdaldeen <abdelrahman.abdaldeen@mawdoo3.com>
2022-05-29 11:22:54 +0300

    performance enhancement

--- middleware/checkToken.js
+++ middleware/checkToken.js
@@ -1,5 +1,8 @@
 export default async function ({ store, app, $axios, redirect, route }) {
   const token = app.$cookies.get('authToken')
+  if (token && store.state.user.displayName) {
+    return;
+  }
   if (token) {
     await $axios
       .$get('users/get', {
